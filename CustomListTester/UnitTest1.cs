5edab58334880ecc0d59faa4c20491da3677032b
Corey Rome <coreyrome2@gmail.com>
2017-12-05 10:23:56 -0600

    Initial commit

---
+++ CustomListTester/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomListTester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        
+        public void TestMethod1()
+        {
+           //Test for adding to end of "list"
+
+            //Test for checking the length of the list after an addition
+
+            //Test
+           
+        }
+    }
+}
